da151b844b883a42be7f69731905de6da4e17237
Patrick Hwang <ph416@scarletmail.rutgers.edu>
2025-12-07 00:20:25 -0500

    get_note completed

--- src/get_note.py
+++ src/get_note.py
@@ -3,28 +3,36 @@ import boto3
 import os
 
 
+
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(os.environ["TABLE_NAME"])
 
 def handler(event, context):
     
+    if not event["pathParameters"].get("id"):
 
-    # resp = table.get_item(Key={"userId": user_id, "id": note_id})
-    # item = resp.get("Item")
+        return {
 
-    # if not item:
-    #     return {
-    #         "statusCode": 404,
-    #         "body": "Not found, unfinished here"
-    #     }
+            "statusCode": 400,
+            "body": json.dumps({"message": "ID not found"})
 
+        }
+        
+    data = event["pathParameters"]["id"]
+    key_dict = {"noteId": data}
 
-    return {
-
-
-        "statusCode": 200, 
-        "body": json.dumps({"message": "API is working, final update for test"})
-
-
-    }
-
+    piece = table.get_item(Key=key_dict)  
+   
+   
+    if not piece.get('Item'): 
+        return {
+            "statusCode": 404,
+            "body": json.dumps({"message": "Note not found"})
+        }
+        
+    else:
+        return {
+                "statusCode": 200,
+                "body":json.dumps({"message": "Note found", 
+                                    "note": piece['Item']})
+        }
