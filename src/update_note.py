2d2dff6bfdd0c8b0ffca966bb13fe921e2e26bb2
Patrick Hwang <ph416@scarletmail.rutgers.edu>
2025-12-08 04:09:35 -0500

    Updated yml + completed update

--- src/update_note.py
+++ src/update_note.py
@@ -1,6 +1,8 @@
 import json
 import boto3
 import os
+from datetime import datetime
+
 
 
 dynamodb = boto3.resource("dynamodb")
@@ -8,3 +10,54 @@ table = dynamodb.Table(os.environ["TABLE_NAME"])
 
 def handler(event, context):
     #we are going to do full overwrite, just replace the entire note completely
+    
+    if not event["pathParameters"].get("id"):
+
+        return {
+
+            "statusCode": 400,
+            "body": json.dumps({"message": "ID not found"})
+
+        }
+        
+    data = event["pathParameters"]["id"]
+    key_dict = {"noteId": data}
+
+    piece = table.get_item(Key=key_dict)  
+   
+   
+    if not piece.get('Item'): 
+        return {
+            "statusCode": 404,
+            "body": json.dumps({"message": "Note not found"})
+        }
+        
+    note = json.loads(event["body"])    
+    
+    if not note.get("title") or not note.get("content"):
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"message": "Note title and content can not be empty."})
+        }
+
+    now = datetime.utcnow().isoformat()   
+    
+    item = {
+        "noteId": data,
+        "title": note["title"],
+        "content": note["content"],
+        "createdAt": piece['Item']['createdAt'],
+        "updatedAt": now
+        
+    }
+    table.put_item(Item=item)
+    
+    return {
+            "statusCode": 200, 
+            "body": json.dumps({"noteId": data,
+                                "title": note["title"],
+                                "content": note["content"],
+                                "createdAt": piece['Item']['createdAt'],
+                                "updatedAt": now,
+                                "message": "Note Updated"})
+                            }
