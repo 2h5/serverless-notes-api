1e00bb807665de8c11aff01d6de5749c515367f4
Patrick Hwang <ph416@scarletmail.rutgers.edu>
2025-12-08 03:22:51 -0500

    Yml updated + completed list notes

--- src/list_notes.py
+++ src/list_notes.py
@@ -1,10 +1,46 @@
 import json
 import boto3
 import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(os.environ["TABLE_NAME"])
 
 def handler(event, context):
-    #list all notes
+  
+    scan = table.scan()
+    
+    scanned = scan["Items"]
+    tables = []
+    for items in scanned:
+        tables.append(items)
+        
+    print(tables)
+
+    cleaned = []
+    
+    
+    for note in tables:
+        title = note["title"]
+        noteid = note["noteId"]
+        
+        
+        new = {
+            "title": title,
+            "noteId": noteid
+        }
+        cleaned.append(new)
+        
+    print(new)
+    print(cleaned)
+    return {
+        
+        "statusCode": 200,
+        "body": json.dumps({"message": cleaned})
+    }
+    
+  
